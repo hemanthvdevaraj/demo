ea3cf9553059903039e83c00c7ad6a8185d5cb46
hemanthvdevaraj <hemanthvdevaraj@gmail.com>
2022-06-05 20:51:26 +0530

    added rest template changes

--- src/main/java/com/play/demo/controller/InventoryController.java
+++ src/main/java/com/play/demo/controller/InventoryController.java
@@ -3,9 +3,14 @@ package com.play.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.play.demo.dto.UserInfoDTO;
 import com.play.demo.entity.Inventory;
 import com.play.demo.service.InventoryService;
 
@@ -23,6 +28,21 @@ public class InventoryController {
 	public List<Inventory> loadInventory() {
 
 		List<Inventory> inv = invService.loadInventory();
+
+		String url = "http://localhost:8081/userinfo/getuser";
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		String requestJson = "1";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
+
+		UserInfoDTO userInfoDto = restTemplate.postForObject(url, entity, UserInfoDTO.class);
+
+		System.out.println(userInfoDto);
 
 		return inv;
 	}
